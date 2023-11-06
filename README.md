34c604f4cf563a8a3137460e480385e0d17779bd
pavankalvium1 <141708070+pavankalvium1@users.noreply.github.com>
2023-11-06 11:14:17 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# numeron
+# Project Numeron
+
+Project Numeron is a number based game
---
+++ app.js
@@ -0,0 +1,8 @@
+const play = document.getElementById("play-button");
+
+play.onclick = () => {
+  location.href = "./game.html";
+};
+
+// Iteration 1: Make the Play button functional.
+// Description: When the Play button is clicked the game page should be displayed.
